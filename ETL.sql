dde96c75d303f031de925d4285659f416215e4be
Derek Malone <79818222+dgmalone@users.noreply.github.com>
2021-07-20 10:40:32 -0700

    Move sequence alters to the ETL process

--- ETL.sql
+++ ETL.sql
@@ -6,3 +6,8 @@ COPY photos(id, review_id, url) FROM '/Users/derekmalone/temp/gimli/Reviews/data
 COPY characteristics(id, product_id, name) FROM '/Users/derekmalone/temp/gimli/Reviews/data/Test/characteristics.csv' DELIMITER ',' CSV HEADER;
 
 COPY characteristics_reviews(id, characteristics_id, review_id, value) FROM '/Users/derekmalone/temp/gimli/Reviews/data/Test/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
+
+Alter sequence reviews_id_seq restart with 5774953;
+Alter sequence photos_id_seq restart with 2742541;
+Alter sequence characteristics_id_seq restart with 3347680;
+Alter sequence characteristics_reviews_id_seq restart with 19327576;
